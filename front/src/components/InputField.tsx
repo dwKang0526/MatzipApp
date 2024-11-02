18237737f9ef9726887e47d2dd368e8dc29604c7
kdw <dwkang0526@gmail.com>
2024-11-02 17:19:46 +0900

    Input 공통 컴포넌트 구현

---
+++ front/src/components/InputField.tsx
@@ -0,0 +1,77 @@
+import React, {useRef} from 'react';
+import {Dimensions, StyleSheet, Text, TextInputProps, View} from 'react-native';
+import {Pressable, TextInput} from 'react-native-gesture-handler';
+import {colors} from '../constants';
+
+interface InputFieldProps extends TextInputProps {
+  disabled?: boolean;
+  error?: string;
+  touched?: boolean;
+}
+
+const deviceHeight = Dimensions.get('screen').height;
+
+function InputField({
+  disabled = false,
+  error,
+  touched,
+  ...props
+}: InputFieldProps) {
+  const innerRef = useRef<TextInput | null>(null);
+
+  // 입력창 클릭 범위 확장 ("이메일을 입력하세요", "비밀번호를 입력하세요" 클릭 시 입력창에 포커스)
+  const handlePressInput = () => {
+    innerRef.current?.focus();
+  };
+
+  return (
+    <Pressable onPress={() => innerRef.current?.focus()}>
+      <View
+        style={[
+          styles.container,
+          disabled && styles.disabled,
+          touched && Boolean(error) && styles.inputError,
+        ]}>
+        <TextInput
+          ref={innerRef}
+          editable={!disabled}
+          placeholderTextColor={colors.GRAY_500}
+          style={[styles.input, disabled && styles.disabled]}
+          autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
+          {...props}
+        />
+        {touched && Boolean(error) && <Text style={styles.error}>{error}</Text>}
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    padding: deviceHeight > 700 ? 15 : 10,
+  },
+  input: {
+    fontSize: 16,
+    color: colors.BLACK,
+    padding: 0,
+  },
+  disabled: {
+    backgroundColor: colors.GRAY_200,
+    color: colors.GRAY_700,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: colors.RED_300,
+  },
+  error: {
+    color: colors.RED_500,
+    fontSize: 12,
+    paddingTop: 5,
+  },
+});
+
+export default InputField;
--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,17 +1,28 @@
-import { StackScreenProps } from '@react-navigation/stack';
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, StyleSheet, View} from 'react-native';
+import {Button, Dimensions, Image, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import { AuthStackParamList } from '../../navigations/stack/AuthStackNavigator';
-import { authNavigations } from '../../constants/navigations';
+import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
+import {authNavigations} from '../../constants/navigations';
 import CustomButton from '../../components/CustomButton';
 
-type AuthHomeScreenProps = StackScreenProps<AuthStackParamList, typeof authNavigations.AUTH_HOME>;
+type AuthHomeScreenProps = StackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME
+>;
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          // resizeMode : 디바이스 크기에 맞게 이미지 크기 조절
+          resizeMode="contain"
+          style={styles.image}
+          source={require('../../assets/matzip.jpeg')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
         <CustomButton
           label="로그인하기"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
@@ -26,6 +37,24 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+  },
+});
 
 export default AuthHomeScreen;
